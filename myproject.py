07b69ca2882225898c1f131df1173a3c3d99d7f7
lavi2211 <117894758+lavi2211@users.noreply.github.com>
2023-09-12 18:46:34 +0530

    new file added

---
+++ myproject.py
@@ -0,0 +1,62 @@
+num=int(input("enter the number :"))
+n1,n2=0,1
+sum=0
+if num<=0:
+    print('please enter the number greater than 0')
+else:
+    for i in range(0,num):
+        print(sum,end=" ")
+        n1=n2
+        n2=sum
+        sum=n1+n2
+
+#new
+n=int(input("enter a number:"))
+n1,n2=0,1
+sum=0
+if n<=0:
+    print("pls enter the number greter than zero")
+else:
+    for i in range(0,n):
+        print(sum,end=" ")
+        n1=n2
+        n2=sum
+        sum=n1+n2
+
+#n
+n=int(input("enter a:"))
+n1,n2=0,1
+sum=0
+if n<=0:
+    print("pls enter a num great than zero")
+else:
+    for i in range(0,n):
+        print(sum,end=" ")
+        n1=n2
+        n2=sum
+        sum=n1+n2
+#s
+n=int(input("entar:"))
+n1,n2=0,1
+sum=0
+if n<=0:
+    print("pls enter a num graeter than zero")
+else:
+    for i in range(0,n):
+        print(sum,end=" ")
+        n1=n2
+        n2=sum
+        sum=n1+n2
+print("="*100)
+#
+n=int(input("en:"))
+n1,n2=0,1
+sum=0
+if n<=0:
+    print("pls enter a num greater than zero")
+else:
+    for i in range(0,n):
+        print(sum,end=" ")
+        n1=n2
+        n2=sum
+        sum=n1+n2
